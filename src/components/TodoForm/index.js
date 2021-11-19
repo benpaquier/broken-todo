08e1c0d395b5bef4730dcc582e7fc5f400d1a2c3
Vincent <vins@Pimentas-MacBook-Air.local>
2021-11-19 15:10:21 +0100

    add task , reset form fixed

--- src/components/TodoForm/index.js
+++ src/components/TodoForm/index.js
@@ -7,28 +7,39 @@ const TodoForm = () => {
   const { todos, setTodos } = useContext(TodoContext)
   const [task, setTask] = useState('')
 
-  const handleAddTodo = () => {
-    // Fin an ability to add new task
+  const handleAddTodo = (e) => {
+    e.preventDefault()
+    if(task.length !==0){
+    setTodos([...todos,
+  {
+    "label": task   
+  }])
+    }
+    setTask("")
   }
+ 
 
   const handleKeyUp = (e) => {
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13) { //press enter to exc handleAddTodo
       handleAddTodo()
+      
     }
+    console.log(e.keyCode);
   }
 
+
   return (
-    <div className="todo-form">
+    <form className="todo-form">
       <input
         placeholder="Enter new task"
         value={task}
         onChange={(e) => setTask(e.target.value)}
         onKeyUp={handleKeyUp}
       />
-      <button type="button" onClick={handleAddTodo}>
+      <button type="submit" onClick={handleAddTodo}>
         Add task
       </button>
-    </div>
+    </form>
   )
 }
 
